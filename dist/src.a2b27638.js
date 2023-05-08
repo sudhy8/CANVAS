e34a99e61b0e80c6e549d726660692b22f324406
sudhy <48507263+sudhy8@users.noreply.github.com>
2023-05-08 09:07:49 +0000

    Update 16 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -177,6 +177,16 @@ module.hot.accept(reloadCSS);
 
 require("./styles.css");
 console.log("hello....");
+var canvas = document.getElementById("canvas");
+if (canvas.getContext) {
+  var ctx = canvas.getContext("2d");
+  ctx.fillRect(10, 10, 300, 300);
+  ctx.clearRect(50, 50, 50, 50);
+  ctx.fillRect(5, 5, 60, 60);
+  ctx.clearRect(25, 25, 50, 50);
+  ctx.strokeRect(30, 30, 30, 30);
+  ctx.clearRect(0, 0, 500, 500);
+}
 },{"./styles.css":"src/styles.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -202,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35205" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40807" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
